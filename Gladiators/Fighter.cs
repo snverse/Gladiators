a2855a6e65647ef0a062b6759db7d9e4ae4f1ff2
snverse <tinverse@gmail.com>
2018-04-30 23:05:01 -0400

    Adding project files

---
+++ Gladiators/Fighter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
+using System.Text;
+
+namespace Gladiators
+{
+    class Fighter
+    {
+        private enum WeaponType
+        {
+            Sword,
+            TwoHandedSword,
+            Spear,
+            Axe
+        };
+
+        private enum OffhandType
+        {
+            Shield,
+            Net,
+            Dagger
+        };
+
+        private uint _health;
+        private WeaponType _weapon;
+        private OffhandType _offhand;
+
+        // constructor
+        private Fighter(string userWep, string userOff)
+        {
+            // set health
+            _health = 100;
+
+            // set weapon
+            try
+            {
+                _weapon = Enum.TryParse(userWep, out WeaponType, _weapon);
+            }
+            catch (Exception e)
+            {
+                _weapon = WeaponType.Sword;
+            }
+            
+            // set offhand
+            try
+            {
+                _offhand = OffhandType.;
+            }
+            catch (Exception e)
+            {
+                _offhand = OffhandType.Shield;
+            }
+        }
+
+        public static SetDamage(uint damage)
+        {
+
+        }
+
+        public static string GetWeaponList()
+        {
+            string list = "";
+            foreach(string weapon in Enum.GetNames(typeof(WeaponType)))
+            {
+                list += weapon + "\n";
+            }
+            return list;
+        }
+
+        public static string GetOffhandList()
+        {
+            string list = "";
+            foreach (string weapon in Enum.GetNames(typeof(OffhandType)))
+            {
+                list += weapon + "\n";
+            }
+            return list;
+        }
+    }
+}
---
+++ Gladiators/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Dynamic;
+
+namespace Gladiators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorText("Welcome to Gladiators!", ConsoleColor.DarkYellow);
+            Fighter playerGladiator;
+            Fighter systemGladiator;
+
+            // game reactor loop
+            while (true)
+            {
+                ColorText("Press any key to start the game.", ConsoleColor.Red);
+                Console.ReadKey();
+                playerGladiator = CreateFighter();
+                //systemGladiator = GenerateFigther();
+
+            }
+        }
+
+        // output in non default color and reset
+        static void ColorText(string text, ConsoleColor textColor = ConsoleColor.White)
+        {
+            Console.ForegroundColor = textColor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        // prompt user for input to build Fighter
+        private static Fighter CreateFighter()
+        {
+            // weapon selection
+            ColorText("What weapon would you like to use?", ConsoleColor.Yellow);
+            Console.WriteLine("Available Weapons: ");
+            Console.WriteLine(Fighter.GetWeaponList());
+            Prompt();
+
+            return Fighter();
+            //offhand selection
+        }
+
+        // auto populate and return Fighter
+        /*private static Fighter GenerateFighter()
+        {
+
+        }*/
+
+        static void Prompt()
+        {
+            ColorText(">", ConsoleColor.Green);
+        }
+    }
+}
