a20eaa1e322eb0e901e420eb1c6ed96719461f84
snverse <tinverse@gmail.com>
2018-05-03 17:06:53 -0400

    First working version

--- Gladiators/Fighter.cs
+++ Gladiators/Fighter.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO.Pipes;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Gladiators
 {
@@ -23,42 +26,126 @@ namespace Gladiators
             Dagger
         };
 
-        private uint _health;
+        Random rnd = new Random();
+        private int _health;
         private WeaponType _weapon;
         private OffhandType _offhand;
 
-        // constructor
-        private Fighter(string userWep, string userOff)
+        // static constructor - random fighter
+        public Fighter()
         {
             // set health
-            _health = 100;
+            SetHealth(100);
 
             // set weapon
-            try
-            {
-                _weapon = Enum.TryParse(userWep, out WeaponType, _weapon);
-            }
-            catch (Exception e)
-            {
-                _weapon = WeaponType.Sword;
-            }
-            
+            SetWeapon(RandomEnumValue<WeaponType>());
+
             // set offhand
-            try
-            {
-                _offhand = OffhandType.;
-            }
-            catch (Exception e)
-            {
-                _offhand = OffhandType.Shield;
-            }
+            SetOffhand(RandomEnumValue<OffhandType>());
         }
 
-        public static SetDamage(uint damage)
+        // constructor
+        public Fighter(string userWep, string userOff)
         {
+            // set health
+            SetHealth(100);
 
+            // set weapon
+            SetWeapon(userWep);
+
+            // set offhand
+            SetOffhand(userOff);
         }
 
+        // attempts to assign weapon, returns true on success
+        private bool SetWeapon(string weaponName)
+        {
+            foreach (var weapon in Enum.GetValues(typeof(WeaponType)))
+            {
+                if (weapon.ToString() == weaponName)
+                {
+                    _weapon = Enum.Parse<WeaponType>(weapon.ToString());
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool SetWeapon(WeaponType weapon)
+        {
+            _weapon = weapon;
+            return true;
+        }
+
+        // attempts to assign offhand, returns true on success
+        private bool SetOffhand(string offhandName)
+        {
+            foreach (var offhand in Enum.GetValues(typeof(OffhandType)))
+            {
+                if (offhand.ToString() == offhandName)
+                {
+                    _offhand = Enum.Parse<OffhandType>(offhand.ToString());
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool SetOffhand(OffhandType offhand)
+        {
+            _offhand = offhand;
+            return true;
+        }
+
+        // get random enum value
+        private static T RandomEnumValue<T>()
+        {
+            var v = Enum.GetValues(typeof(T));
+            return (T) v.GetValue(new Random().Next(v.Length));
+        }
+
+        private void SetHealth(int health)
+        {
+            _health = health;
+        }
+
+        // get damage roll from fighter
+        public int GetDamage()
+        {
+            switch (_weapon)
+            {
+                case WeaponType.Sword:
+                    return rnd.Next(2, 6);
+                case WeaponType.Axe:
+                    return rnd.Next(0, 10);
+                case WeaponType.Spear:
+                    return rnd.Next(3, 7);
+                case WeaponType.TwoHandedSword:
+                    return rnd.Next(0, 10);
+            }
+
+            return 0;
+        }
+
+        // register damage
+        public void SetDamage(int damage)
+        {
+            _health -= damage;
+        }
+
+        // get hp
+        public int getHealth()
+        {
+            return _health;
+        }
+
+        // get equips seperated by " and "
+        public string GetEquips()
+        {
+            return _weapon.ToString() + " and " +_offhand.ToString();
+        }
+
+        // return string of weapons seperated by '\n'
         public static string GetWeaponList()
         {
             string list = "";
@@ -69,6 +156,7 @@ namespace Gladiators
             return list;
         }
 
+        // return string of offhands seperated by '\n'
         public static string GetOffhandList()
         {
             string list = "";
--- Gladiators/Program.cs
+++ Gladiators/Program.cs
@@ -5,20 +5,74 @@ namespace Gladiators
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            ColorText("Welcome to Gladiators!", ConsoleColor.DarkYellow);
-            Fighter playerGladiator;
-            Fighter systemGladiator;
-
             // game reactor loop
             while (true)
             {
-                ColorText("Press any key to start the game.", ConsoleColor.Red);
-                Console.ReadKey();
-                playerGladiator = CreateFighter();
-                //systemGladiator = GenerateFigther();
+                ColorText("Welcome to Gladiators!", ConsoleColor.Green);
+                ColorText("Press L key to start a local game.", ConsoleColor.Yellow);
+                ColorText("Press X key to exit the game.", ConsoleColor.Yellow);
+                string keyPress = Prompt();
 
+                switch (keyPress)
+                {
+                    case "x":
+                    case "X":
+                        ColorText("Thank you for playing!", ConsoleColor.Magenta);
+                        System.Threading.Thread.Sleep(2000);
+                        return 0;
+
+                    case "l":
+                    case "L":
+                        LocalGame();
+                        break;
+                }
+            }
+        }
+
+        private static void LocalGame()
+        {
+            ColorText("The available weapons are:", ConsoleColor.Yellow);
+            ColorText(Fighter.GetWeaponList(), ConsoleColor.Yellow);
+            string weapon = Prompt();
+
+            ColorText("The available offhands are:", ConsoleColor.Yellow);
+            ColorText(Fighter.GetOffhandList(), ConsoleColor.Yellow);
+            string offhand = Prompt();
+
+            Fighter playerGladiator = new Fighter(weapon, offhand);
+            Fighter systemGladiator = new Fighter();
+
+            ColorText(string.Format("You're using:\n{0}.", playerGladiator.GetEquips()), ConsoleColor.Yellow);
+            ColorText(string.Format("Enemy is using:\n{0}.", systemGladiator.GetEquips()), ConsoleColor.Yellow);
+
+            while (true)
+            {
+                // Chat  phase
+                string enemyReport = string.Format("ENEMY: {0}", systemGladiator.getHealth());
+                string hpReport = string.Format("HP: {0}", playerGladiator.getHealth());
+
+                ColorText("------------------------------", ConsoleColor.Red);
+                ColorText(enemyReport, ConsoleColor.Yellow);
+                ColorText(hpReport, ConsoleColor.Yellow);
+
+                // Damage phase
+                systemGladiator.SetDamage(playerGladiator.GetDamage());
+                if (systemGladiator.getHealth() < 1)
+                {
+                    ColorText("YOU SLAYED THE ENEMY!", ConsoleColor.Green);
+                    return;
+                }
+
+                playerGladiator.SetDamage(systemGladiator.GetDamage());
+                if (playerGladiator.getHealth() < 1)
+                {
+                    ColorText("YOU GOT SLAYED", ConsoleColor.Red);
+                    return;
+                }
+
+                Console.ReadKey();
             }
         }
 
@@ -30,28 +84,12 @@ namespace Gladiators
             Console.ResetColor();
         }
 
-        // prompt user for input to build Fighter
-        private static Fighter CreateFighter()
+        private static string Prompt()
         {
-            // weapon selection
-            ColorText("What weapon would you like to use?", ConsoleColor.Yellow);
-            Console.WriteLine("Available Weapons: ");
-            Console.WriteLine(Fighter.GetWeaponList());
-            Prompt();
-
-            return Fighter();
-            //offhand selection
-        }
-
-        // auto populate and return Fighter
-        /*private static Fighter GenerateFighter()
-        {
-
-        }*/
-
-        static void Prompt()
-        {
-            ColorText(">", ConsoleColor.Green);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("> ");
+            Console.ResetColor();
+            return Console.ReadLine();
         }
     }
 }
